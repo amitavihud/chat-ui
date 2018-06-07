39ebc32c39acc35d7a2629f8fc6d9dcb64e059d8
Amit Avihud <amitavihud@Amits-MacBook-Pro.local>
2018-06-07 14:48:12 +0300

    message input

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export default styled.div`
-  border: 1px solid black;
   display: grid;
   height: 100%;
   box-sizing: border-box;
   grid-template-columns: auto;
-  grid-template-rows: auto 100px;
+  grid-template-rows: auto 150px;
   background-color: #DEE1EB;
 `
--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,5 +1,75 @@
-import styled from "styled-components";
-
-export default styled.div`
-  border: 1px solid blue;
+import React from 'react'
+import styled, { css } from "styled-components";
+import logo from './assets/002-psyduck.png'
+const Container = styled.div`
+  padding: 10px;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: auto auto auto;
+  
+  .avatar {
+    width: 20px;
+    align-self: center;
+    justify-self: center;
+  }
 `
+const inputsStyle = css`
+  background-color: #F1F1F7;
+  color: #858DA8;
+  border: none;
+  border-radius: 3px;
+  outline-color: transparent;
+  outline-style: none;
+  width: 100%;
+  box-sizing: border-box;
+  &::placeholder {
+    color: #c4cadc
+  }
+`
+
+const Input = styled.input`
+  ${inputsStyle}
+  padding: 2px 9px;
+`
+const Message = styled.textarea`
+  ${inputsStyle}
+  padding: 10px;
+  margin-top: 5px;
+  resize: none;
+`
+const NameInputContainer = styled.div`
+  display: grid;
+  grid-template-columns: 30px auto;
+`
+const SendContainer = styled.div`
+  justify-self: right;
+`
+const Send = styled.button`
+  padding: 8px 15px;
+  margin: 10px;
+  background-color: #50BC7E;
+  color: white;
+  border-radius: 5px;
+  outline-style: none;
+
+  &:active {
+    background-color: #289154;
+  }
+`
+
+export default class MessageInput extends React.Component {
+  render() {
+    return (
+      <Container>
+        <NameInputContainer>
+          <img className="avatar" src={logo} />
+          <Input placeholder="Your name" value="Amit Avihud" />
+        </NameInputContainer>
+        <Message placeholder="Message" />
+        <SendContainer>
+          <Send>Send</Send>
+        </SendContainer>        
+      </Container>
+    )
+  }
+}
